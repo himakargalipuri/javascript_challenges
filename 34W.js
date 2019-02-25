e8a3051c0bfdb540ef9acd86b5087be46eb08be6
udaykumar <uday.sivapuramvenkata@mobigesture.com>
2019-02-25 22:35:58 +0530

    Printing the Current Window.

---
+++ 34W.js
@@ -0,0 +1,13 @@
+//print the contents of the current window
+//Expected input type : calling the default function window.print();
+
+const printCurrent = () => {
+  return ( true ? printCurr() : "Failed to Execute because of Wrongly Entered Input"); 
+};
+
+printCurrent();
+
+function printCurr()
+{
+  return window.print();
+}
