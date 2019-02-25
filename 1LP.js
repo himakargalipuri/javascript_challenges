948800decef70f085608ef00f1af938ab32c79e4
udaykumar <uday.sivapuramvenkata@mobigesture.com>
2019-02-25 18:45:25 +0530

    Factorial of a Number.

---
+++ 1LP.js
@@ -0,0 +1,29 @@
+//Return factorial of a number
+//Expected input type : number (5, 10, 2, ...)
+
+var input = 5;
+
+//Mention Comments at  each line saying what it does.
+const factorial = (input) => {
+return (typeof input == 'number' ) ? fact(input) : "Failed to Execute because of Wrongly Entered Input";
+    
+};
+var res = factorial(input);
+
+function fact(input) {
+  
+  var fct = 1;
+//Checking here whether the number is between 0 to 100.
+  if (input > 0 && input < 100) {
+    //Running the loop to find the factorial
+    for (var i = 1; i <= input; ++i)
+      fct = fct * i;
+
+    console.log(" Factorial of " + input + " is " + fct);
+  }
+  else {
+    //throwing the error given number is not in range 0 - 100!
+    throw new Error("Number Provided is not in range 0 - 100!");
+  }
+}
+//Expected output for given input is Factorial of 5 is 120.
