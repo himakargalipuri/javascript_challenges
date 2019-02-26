e8c4e057cd6c80e2e2f867c61998063dd48b0e24
Abhinav <chelluri.abhinav@gmail.com>
2019-02-26 14:59:34 +0530

    19. Given an array of objects with names and ages. Return Segregated names in an array (extra points sort by age ) [{‘name’: ‘john’, ‘age’:25},{‘name’:’ram’, ‘age’:20}] = [‘john’, ‘ram’]

--- 19.js
+++ 19.js
@@ -3,13 +3,14 @@
 // [{‘name’: ‘john’, ‘age’:25},{‘name’:’ram’, ‘age’:20}] = [‘john’, ‘ram’] 
 
 var arr = [{"name": "john", "age": 20}, {"name": "ram", "age": 25}, {"name": "ap", "age": 18}] 
+
 if(!(arr instanceof Array) || arr == "") {  //checking if array or not
     console.log("Expected Array, Got something else");
   } else {
     for (let i in arr){ //checking if array of objets or not
         return (arr[i] instanceof Object) ? console.log((arr.sort((a,b) => (b.age - a.age))).map((x) => x.name)) : console.log(("Please enter an array of objects"));
+        // sorting by age and retrieving Segregated names using map
     }
-    // sorting by age and retrieving Segregated names using map
   }
 
     //conclusion: 
