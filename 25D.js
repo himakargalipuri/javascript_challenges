af8dda1b3f0c8e680f12e2daf33e16bbef5e2b05
Akhil <uday.sivapuramvenkata@mobigesture.com>
2019-02-26 11:41:47 +0530

    Get current date time and convert it into.

---
+++ 25D.js
@@ -0,0 +1,24 @@
+//Get current date time and convert it into Today is 18 February 2019. Its 4 hours 15 minutes and 13 seconds now
+//Expected input type : calling the default functions...
+
+const currentDate = () => {
+  var dateData = new Date();
+  var date = dateData.getDate();
+  var month = dateData.getMonth();
+  var year = dateData.getFullYear();
+  var hours = dateData.getHours();
+  var minutes = dateData.getMinutes();
+  var seconds = dateData.getSeconds();
+  
+  var arr = ["January","Feburary","March","April","may","June","July","August","September","October","November","December"];
+  
+  console.log("Today is " + date + " " + arr[month] + " " + year + " " + ". Its " + hours + " hours " + minutes + " minutes and " + seconds + " seconds now");
+ }
+
+const PrntDate = () => {
+  return (true)  ? currentDate() : "Failed to Execute because of Wrongly Entered Input";
+ };
+  
+PrntDate();
+  
+  //Expected Output for the given input calling function PrntDate is Today is 25 Feburary 2019 . Its 19 hours 52 minutes and 52 seconds now
