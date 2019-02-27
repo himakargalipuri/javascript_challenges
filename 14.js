e8e950383d0d8b832589b299d9f5d32b0a97fba5
Abhinav <chelluri.abhinav@gmail.com>
2019-02-27 19:11:18 +0530

    Given an array of numbers, add every item and return sum

--- 14.js
+++ 14.js
@@ -1,17 +1,20 @@
 //14. Given an array of numbers, add every item and return sum 
 var arr = [2,5,3,5,2,3,1,4]
 
-//checking valid input
-if(!(arr instanceof Array) || arr == "") {  //checking if array or not
-  console.log("Expected Array, Got something else");
-} else {
+const addEach = (arr) => {
   console.log("Sum of Array Elements: " + arr.reduce((s, item) => s + item));
   //using reduce to go through each element and adding items in every pass to s, i.e s+=item
 }
-        
+
+const  init = () => {
+  ((arr == "") || !(arr instanceof Array)) ? console.log("Expected array, Got something else") : addEach(arr);
+  //checking the input value
+}
+init();
+
   //conclusion: 
   // Checked with [2,5,3,5,2,3,1,4], "2,5,3,5,2,3,1,4", true/false,  {"a": 5, "b": 6, "c": 8, "b": 6, "a": 5,"c": 8} 
   
   //Input: [2,5,3,5,2,3,1,4]       => Output: Sum of Array Elements: 25 
-  //Input: other than array        => Outpt: Expected Number, Got something else
+  //Input: other than array        => Output: Expected array, Got something else
   
