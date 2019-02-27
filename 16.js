ce8ada79c4e1a7c2257fbd112a9368676983d8df
Abhinav <chelluri.abhinav@gmail.com>
2019-02-27 23:35:25 +0530

    Given a number of 10 digits (0000000000) convert it “+1 (000) 000-0000” format

--- 16.js
+++ 16.js
@@ -3,13 +3,16 @@
 var num = 1234567890;
 var arr = Array.from(num.toString().split(""));    //converting the number to string then to array
 
+const convert = (num) =>{
+  console.log("+1 ("+ arr[0]+arr[1]+arr[2] +") " + arr[3]+arr[4]+arr[5] +  "-" + arr[6]+arr[7]+arr[8]+arr[9])
+}
 //checking valid input
 if(!(typeof num == 'number') || num==""){  //checking if number is valid or not
   console.log("Expected Number, Got something else");
 } else if((num >=10000000000) || num <= 999999999){       //checking between the range
     console.log("Please enter excatly 10 digits and Make sure that the starting digit is not 0");
 } else {
-  console.log("+1 ("+ arr[0]+arr[1]+arr[2] +") " + arr[3]+arr[4]+arr[5] +  "-" + arr[6]+arr[7]+arr[8]+arr[9])
+  convert(num)
 }
 
   //conclusion: 
