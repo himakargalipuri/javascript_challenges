267b9d8347b449828a2269c5766e605ad0e99e78
Akhil <uday.sivapuramvenkata@mobigesture.com>
2019-02-27 16:10:12 +0530

    Given annual salary numbers, calculate income tax rate and income tax amount Explore income tax slabs.

--- 23C.js
+++ 23C.js
@@ -1,36 +1,50 @@
 //Given annual salary numbers, calculate income tax rate and income tax amount Explore income tax slabs
-//Expected input type : income (Enter the income)
+//Expected input type : Number (Income amount.)
 
-var income = 10000;
+var income = 300000;
 //Tried with different inputs like "akhil",{"Akhil" : 123},["akhi",123], true...
 
 //This loop Calculates the Tax rate
-const calculate_tax = (amnt) => {
-  var rate = (amnt / income) * 100;
-return rate ;
+const calculate_rate = (income,tax) => {
+  var rate = 0;
+    if(income < 250000)
+    {
+      rate = "nill";
+    }
+    else if(income > 250001 && income < 500000){
+      rate = ( (tax/(income - 250000))*100 );
+    }
+    else if(income > 500001 && income < 1000000){
+      rate = ( (tax/(income - 500000))*100);  
+    }
+    else{
+        rate = ( (tax/(income - 1000000))*100);
+    }
+     return rate;
 }
 
 //This loop Calculates the Tax Amount
-const calcTaxes = (amount) => {
-  var calculate = 0;
-  if(amount > 85650){
-      tax = (amount - 85650) * .28 + 870.0+ (35350-8700)*.15 + (89350-36900)*.25;
-  }
-  else if( amount > 35350){
-  tax = (amount - 35350) * .25 + 870.0+(35350-8700)*.15;    
-  }
-  else if(amount > 8700){
-  tax = (amount - 8700) * .15 + 870.0;
-  }
-  else{
-     tax = amount * .10;
-      }
-  tax += amount * .153;
-   return tax;
+  const calcTaxes = (income) => {
+    var tax = 0;
+    if(income < 250000)
+    {
+      tax = "nill";
+    }
+    else if(income > 250001 && income < 500000){
+      tax = ( (income - 250000)*5/100 );
+    }
+    else if(income > 500001 && income < 1000000){
+      tax = ( ((income - 500000)*20/100) + 12500 );  
+    }
+    else{
+        tax = ( ((income - 1000000)*30/100) + 112500 );
+    }
+     return tax;
 }
-
-const Taxrate = (amnt) => {
-  return ( (typeof income == 'number') ? calculate_tax(amnt) : "Failed to Execute because of Wrongly Entered Input"); 
+  
+  
+const Taxrate = (income,tax) => {
+  return ( (typeof income == 'number') && (typeof tax == 'number') ? calculate_rate(income,tax) : "Failed to Execute because of Wrongly Entered Input"); 
 };
 
 const Taxamount = (income) => {
@@ -39,13 +53,12 @@ const Taxamount = (income) => {
 
 //Calling the Tax Amount Calculating Fucnction and the tax rate calculating function here
 var amnt = Taxamount(income) ;
-var rte = Taxrate(amnt) ;
+var rte = Taxrate(income,amnt) ;
 
 console.log("The Income Tax amount for Given Income " + income + " is " +  amnt);
-console.log("The Income Tax rate for Given Income " + income + " is " +  rte);
+console.log("The Income Tax rate for Given Income " + income + " is " +  rte + "%.");
 
-
-//Expected output when the income is given 10000 is The Income Tax amount for Given Income 10000 is 2595 The Income Tax rate for Given Income 10000 is 25.95
+//Expected output when the income is given 300000 is The The Income Tax amount for Given Income 300000 is 2500 The Income Tax rate for Given Income 300000 is 5%.
 
 //Expected output when the income is given boolen is The Income Tax amount for Given Income true is Failed to Execute because of Wrongly Entered Input
 
