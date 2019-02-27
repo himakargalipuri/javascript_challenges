b790a41088f616d9b82a4f8f0ecdf956a9ad2a4e
Akhil <uday.sivapuramvenkata@mobigesture.com>
2019-02-27 17:19:29 +0530

    Sort an array of strings based on Length, Length of vowels, Alphabetical.

--- 4A.js
+++ 4A.js
@@ -5,7 +5,7 @@
 
 //Expected input type : Array with Strings in it.
 
-var arr = ["akhil","asd","raj","challenges","javaScript"];
+var arr = ["akhilee","asapu","rajii","ceehalleinges","javaScript"];
 //Tried with different inputs like  123,{"Akhil" : 123},["akhi","Akhilraj"],true...
 
 //This function is to make every 4th char as 4 the given string...
@@ -18,28 +18,15 @@ const sortByLength = (arr) =>{
 }
   
 //This function is to sort given array of strings by Length of vowels.
-const sortByVowelsLen = (arr) => { //Facing Problem here to sort the given array based on vowel length
-  
-  for(var i = 0; i < arr.length; i++)
-    { 
-      let Vcount = 0;
-      for(var j = 0; j < arr[i].length; j++)
-        {
-          ch = arr[i].charAt(j);
-          // console.log(arr[i]);
-          if(ch.match(/[aeiou]/i))
-          {
-              Vcount++;
-          }
-        }
-      console.log("Length of the Vowels in " + (arr[i]) + "th string is " + Vcount);
-    }
-  // console.log("Array sorted by Length of the Vowels in the string is " + arr2);
+const sortByVowelsLen = (arr) => { 
+  var arr2 = arr.sort( (a,b) =>(a.match(/[aeiou]/i).length - b.match(/[aeiou]/i).length));
+  console.log("Array sorted by Length of the vowels is " + arr2 );
 }
-  //This function is to sort given array of strings in Alphabetical order.
+  
+//This function is to sort given array of strings in Alphabetical order.
   const sortByAlpha = (arr) =>{
-    var arr2 = arr.sort();
-    console.log("Array sorted by Alphabetical order of the elements is " + arr2);
+    var arr3 = arr.sort();
+    console.log("Array sorted by Alphabetical order of the elements is " + arr3);
   }
   
   sortByLength(arr);
@@ -54,8 +41,7 @@ const stngArray = (arr) => {
 //Calling the function here
 stngArray(arr) ;
 
-//Expected output when the Given Array with Strings is Array sorted by Length of the elements is asd,raj,akhil,avaScript,challenges 
-//Array sorted by Alphabetical order of the elements is akhil,asd,avaScript,challenges,raj
+//Expected output when the Given Array with Strings is Array sorted by Length of the elements is asapu,rajii,akhilee,javaScript,ceehalleinges...  Array sorted by Length of the vowels is asapu,rajii,akhilee,javaScript,ceehalleinges...  Array sorted by Alphabetical order of the elements is akhilee,asapu,ceehalleinges,javaScript,rajii...
 
 //Expected output when the Given input is 123 Failed to Execute because of Wrongly Entered Input
 
