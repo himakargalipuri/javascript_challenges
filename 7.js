14e47b9815eb96c1122809898b4c8353fd56fcd0
Abhinav <chelluri.abhinav@gmail.com>
2019-02-27 17:43:18 +0530

    Eliminate odd numbers in an array

--- 7.js
+++ 7.js
@@ -1,17 +1,21 @@
 //7. Eliminate odd numbers in an array 
 var arr = [2,5,3,1,4]
 
-//checking valid input
-if(!(arr instanceof Array) || arr == ""){
-  console.log("Expected Array, Got something else");
-} else {
+const removeOdd = (arr) =>{
   console.log("After removing Odd Numbers: " + arr.filter((x) => (x%2==0)).sort((a,b) => (a-b)));
   //using filter to go through each element and applying conditions
 }
-        
+
+//checking valid input
+const  init = () => {
+  ((arr == "") || !(arr instanceof Array)) ? console.log("Expected array, Got something else") : removeOdd(arr);
+  //checking the input value
+}
+init();
+
   //conclusion: 
-  // Checked with [2,5,3,1,4], "2,5,3,1,4", true/false,  {"a": 5, "b": 6, "c": 8} 
+  // Checked with [2,5,3,1,4], 25314, "2,5,3,1,4", true/false,  {"a": 5, "b": 6, "c": 8} 
   
-  //Input: [1,2,3,5]               => Output: After removing Odd Numbers: 2,4
-  //Input: other than array        => Outpt: Expected Number, Got something else
+  //Input: [2,5,3,1,4]             => Output: After removing Odd Numbers: 2,4
+  //Input: other than array        => Outpt: Expected array, Got something else
   
