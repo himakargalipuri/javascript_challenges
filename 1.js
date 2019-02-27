95a6df146510a823771f5254ef736275d29b30d0
Abhinav <chelluri.abhinav@gmail.com>
2019-02-27 16:04:11 +0530

    Return factorial of a number

--- 1.js
+++ 1.js
@@ -10,20 +10,18 @@ const findFactorial = ((x)=> {
 });
 
 //checking various inputs
-if(num == ""){
-  throw new Error("Number not provided");
-} else if(num < 0 || num >100) {
-  console.log("Enter between 0 and 100");
-} else if(!(typeof num == 'number')) {
-  console.log("Expected Number, Got something else");
-} else { 
-  console.log(findFactorial(num));
+const  init = () => {
+  ((num == "") || !(typeof num == 'number')) ? (console.log("Expected Number as input, Got something else")) : checkRange(num);
 }
 
-        
-//conclusion: 
+const checkRange = (n)=> {
+  return (n > 100 || n < 0) ? console.log("Enter number between 0 and 100") : console.log(findFactorial(num));
+}
+init();
+
+//Conclusion: 
 // Checked with 5, "5", [5], true/false, ((x)=> x)(5);, {"a": 5} 
 
-//Input: 5 or ((x)=> x)(5) IIFE;            => Output: 120
+//Input: 5 or ((x)=> x)(5); IIFE;           => Output: 120
 //Input: other than number                  => Outpt: Expected Number, Got something else
 //Input: Less than 0 and greater than 100   => Output: Enter between 0 and 100
