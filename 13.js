d85345e2bb19ac345309fce36be9ef4f37667acd
Abhinav <chelluri.abhinav@gmail.com>
2019-02-28 00:38:39 +0530

    Given an array of mixed value types. Return an array of their types [1,’adsf’, [a,b], {}, undefined, true] = [number, string, array, object, undefined, boolean]

--- 13.js
+++ 13.js
@@ -3,9 +3,11 @@
 
 var arr = [1, "adsf", {}, undefined, ["a", "b"], true]
 
-
 const typeOf = (arr) => {
-  console.log(arr.map((x) => {return typeof x}))
+  console.log(arr.map((x) => {
+      return (typeof x == 'object' ? ((x instanceof Array) ? "array" : "object") : typeof x )
+    }
+  ))
   //using map to go through each element and returning typeof each element x
 }
 const  init = () => {
@@ -19,6 +21,6 @@ init();
   //When returning the typeof [a,b] it give output as 'object' because Arrays in JavaScript are special case of objects (numbered index objects)
   
   // Checked with [1, "adsf", {}, undefined, ["a", "b"], true]], 1, "adsf", {}, undefined, ["a", "b"], true",  {"a": 1, "b": "asdf" "c": {}, "b": true, "a": undefined} 
-  //Input: [1, "adsf", {}, undefined, ["a", "b"], true]]     => Output: [ 'number', 'string', 'object', 'undefined', 'object', 'boolean' ]
+  //Input: [1, "adsf", {}, undefined, ["a", "b"], true]]     => Output: [ 'number', 'string', 'object', 'undefined', 'array', 'boolean' ]
   //Input: other than array        => Outpt: Expected array, Got something else
   
