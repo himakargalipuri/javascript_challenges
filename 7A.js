0412d90310e4fc1083fbd9b40334b71a7affaad8
Akhil <uday.sivapuramvenkata@mobigesture.com>
2019-02-27 12:50:51 +0530

    Eliminate odd numbers in an array.

--- 7A.js
+++ 7A.js
@@ -1,7 +1,7 @@
 //Eliminate odd numbers in an array
 //Expected input type : Array.
 
-var arr = [1,4,2,7,11,9,6,5,4,2,0,8,6,12];
+var arr = [1,4,2,7,11,9,6,5,4,2,8,6,12];
 //Tried with different inputs like  123,{"Akhil" : 123},"Akhil",true...
 
 //This function is to eliminate the odd numbers from the given Array...
@@ -27,7 +27,7 @@ var res = Oddnumb(arr) ;
 console.log("Array after Eleminating the Odd Numbers is ");
 console.log(res);
 
-//Expected output when the Given array is Array after Eleminating the Odd Numbers is  [4, 2, 6, 4, 2, 0, 8, 6, 12]
+//Expected output when the Given array is Array after Eleminating the Odd Numbers is  [4, 2, 6, 4, 2, 8, 6, 12]
 
 //Expected output when the Given input is 123 Array after Eleminating the Odd Numbers is Failed to Execute because of Wrongly Entered Input
 
