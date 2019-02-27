aa8efc9dedfd8d481318fc2e44a5aee6d9abb184
Abhinav <chelluri.abhinav@gmail.com>
2019-02-27 18:33:03 +0530

    Remove duplicates from array

--- 9.js
+++ 9.js
@@ -1,17 +1,20 @@
 //9. Remove duplicates from array 
-var arr = [2,5,3,5,2,3,1,4]
+var arr = [2,5,3,5,2,3,1,4, "ani", "ani"]
 
-//checking valid input
-if(!(arr instanceof Array) || arr == ""){
-  console.log("Expected Array, Got something else");
-} else {
-  console.log("After removing Duplicate Numbers: " + arr.filter((item, index) => (arr.indexOf(item)>=index)).sort());
+const removeDup = (arr) => {
+  console.log("After removing Duplicates: " + arr.filter((item, index) => (arr.indexOf(item)>=index)).sort());
   //using filter to go through each element and applying conditions
 }
-        
+
+const  init = () => {
+  ((arr == "") || !(arr instanceof Array)) ? console.log("Expected array, Got something else") : removeDup(arr);
+  //checking the input value
+}
+init();
+
   //conclusion: 
-  // Checked with [2,5,3,5,2,3,1,4], "2,5,3,5,2,3,1,4", true/false,  {"a": 5, "b": 6, "c": 8, "b": 6, "c": 8} 
+  // Checked with [2,5,3,5,2,3,1,4,"ani","ani"], "2,5,3,5,2,3,1,4", true/false,  {"a": 5, "b": 6, "c": 8, "b": 6, "c": 8} 
   
-  //Input: [2,5,3,5,2,3,1,4]       => Output: After removing Odd Numbers: 1,2,3,4,5
-  //Input: other than array        => Outpt: Expected Number, Got something else
+  //Input: [2,5,3,5,2,3,1,4,"ani","ani"]     => Output: After removing Duplicates: 1,2,3,4,5,ani
+  //Input: other than array                  => Output: Expected array, Got something else
   
