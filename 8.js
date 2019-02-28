9729c5990508aeaeac6b7591f41def9c0add7603
Abhinav <chelluri.abhinav@gmail.com>
2019-02-28 23:27:34 +0530

    Create classes for square and rectangle to return perimeter and area when given length and breadth

--- 8.js
+++ 8.js
@@ -1,58 +1,60 @@
-//8. Create classes for square and rectangle to return perimeter and area when 
-// given length and breadth 
+//8. Create classes for square and rectangle to return perimeter and area when given length and breadth
 
 var len = 10, br = 2;
-const  init = () => {
-    (len == "" || br == "" || !(typeof len == 'number') || !(typeof br == 'number')) ?
-     console.log("Expected Number, Got something else") : rectSquare(len, br);
-    //checking the input value
-}
 
 const square = (l) => {
     class Square{       //creating Class Square
       constructor(l){
         this.l=l;
       }
-    area(){ return this.l * this.l;}
-    perimeter(){ return this.l * 4;}
+      area(){ return this.l * this.l;}
+      perimeter(){ return this.l * 4;}
     }
     var sq = new Square(l), a, p;
     a = sq.area();
     p = sq.perimeter();
-    console.log("Square: Area = " + a + ", Perimeter = " +p);
+    return ("Square: Area = " + a + ", Perimeter = " +p);
 }
 
 const rectangle = (l, b) => {   
     class Rectangle{            //creating Class Rectangle
-        constructor(l, b){
-            this.l=l;
-            this.b=b;
-        }
-        area(){ return this.l * this.b;}
-        perimeter(){ return 2 * (this.l + this.b)}
+      constructor(l, b){
+        this.l=l;
+        this.b=b;
+      }
+      area(){ return this.l * this.b;}
+      perimeter(){ return 2 * (this.l + this.b)}
     }
-
     var re = new Rectangle(l, b), a, p;
     a = re.area();
     p = re.perimeter();
-    console.log("Rectangle: Area = " + a + ", Perimeter = " +p);
+    return ("Rectangle: Area = " + a + ", Perimeter = " +p);
 }
-//driver code
+
 const rectSquare = (l,b) => {
-    square(l);
-    rectangle(l,b)
+  return square(l) + "\n"+ rectangle(l,b)
 }
 
+const check = (len, br) => {
+  //checking valid number types
+  return (isNaN(len) || len == Infinity || len == -Infinity || isNaN(br) || br == Infinity || br == -Infinity) ? 
+    "Please enter numbers other than NaN, +Infinity, -Infinity" : 
+    rectSquare(len, br);
+}
+const  init = () => {
+(!(typeof len == 'number') || !(typeof br == 'number')) ?
+    console.log("Expected Number, Got something else") : console.log(check(len, br));
+    //checking the input value
+}
 init();
-
      
 //Conclusion: 
 // Checked with len = 10 br = 2, len = "10" br = 2,  len = "10" br = "2",
-//              len = true br= false,  {"len": "5", "br": "6"} 
-  
+//              len = true br= false,  {"len": "5", "br": "6"}, null, undefined, "", Infinity, -Infinity, NaN
+
 // Input: len = 10, br = 2       
 // => Output: Square: Area = 100, Perimeter = 40
 //            Rectangle: Area = 20, Perimeter = 24
-// 
-//Input: other than len = Number and br = Number  => Output: Expected Number, Got something else
+// Input: len = 10, br = Infinity                  => Output: Please enter numbers other than NaN, +Infinity, -Infinity
+// Input: other than len = Number and br = Number  => Output: Expected Number, Got something else
   
