2af0ab74b0b93327e389975f2e0241ee4ab55f94
Abhinav <chelluri.abhinav@gmail.com>
2019-02-28 23:26:55 +0530

    Eliminate odd numbers in an array

--- 7.js
+++ 7.js
@@ -2,20 +2,28 @@
 var arr = [2,5,3,1,4]
 
 const removeOdd = (arr) =>{
-  console.log("After removing Odd Numbers: " + arr.filter((x) => (x%2==0)).sort((a,b) => (a-b)));
-  //using filter to go through each element and applying conditions
+  return "After removing Odd Numbers: " + arr.filter((x) => x%2==0).sort((a,b) => (a-b));
+  //using filter to go through each element and applying condition
 }
 
-//checking valid input
+const check = (arr) => {
+  for(let i in arr){      //checking if array of numbers or not
+    return (!(typeof arr[i] == 'number')||(isNaN(arr[i])) || (arr[i] == Infinity) || (arr[i] == -Infinity)) ? 
+      "Please enter array numbers other than NaN, +Infinity, -Infinity" : 
+      removeOdd(arr);
+  }
+}
+      
 const  init = () => {
-  ((arr == "") || !(arr instanceof Array)) ? console.log("Expected array, Got something else") : removeOdd(arr);
+  (!(arr instanceof Array)) ? console.log("Expected array, Got something else") : console.log(check(arr));
   //checking the input value
 }
 init();
 
   //conclusion: 
-  // Checked with [2,5,3,1,4], 25314, "2,5,3,1,4", true/false,  {"a": 5, "b": 6, "c": 8} 
+  // Checked with [2,5,3,1,4], 25314, [10/"hi",2,"3",5], "2,5,3,1,4", true/false,  {"a": 5, "b": 6, "c": 8}, null, undefined, "", Infinity, -Infinity, NaN  
   
   //Input: [2,5,3,1,4]             => Output: After removing Odd Numbers: 2,4
-  //Input: other than array        => Outpt: Expected array, Got something else
+  //Input: [10/"hi",2,"3",5]       => Output: Please enter array numbers other than NaN, +Infinity, -Infinity
+  //Input: other than array        => Output: Expected array, Got something else
   
