3852c9d04e9313bc1582c2dc42469cad3d5b92fe
Abhinav <chelluri.abhinav@gmail.com>
2019-02-28 23:42:21 +0530

    onvert celsius data to fahrenheit

--- 35.js
+++ 35.js
@@ -1,21 +1,25 @@
 //35. Convert celsius data to fahrenheit 
 
 var num = 120;
-const  init = () => {
-    ((num == "") || !(typeof num == 'number')) ? console.log("Expected Number, Got something else") : console.log(toFar(num));
-    //checking the input value
-}
-//driver code
 const toFar = (n) => {
-    return n + "° Celsius In Fahrenheit: " + (((n * (9/5)) + 32)) + " °F";  //Mathematical Formula
+  return n + "° Celsius In Fahrenheit: " + (((n * (9/5)) + 32)) + " °F";  //Mathematical Formula
+}
+
+const check = (num) => {
+  ((isNaN(num)) || (num == Infinity) || (num == -Infinity)) ? console.log("Please enter number other than NaN, +Infinity, -Infinity") : console.log(toFar(num));
+}
+  
+//checking various inputs
+const  init = () => {
+  !(typeof num == 'number') ? console.log("Expected Number as input, Got something else") : check(num);
 }
 init();
-
      
 //Conclusion: 
-// Checked with -10, "129", [1234], true/false,  {"a": "125", "b": "36", "c": "28"} 
+// Checked with -10, "120", [1234], true/false,  {"a": "125", "b": "36", "c": "28"}, null, undefined, "", NaN, Infinity, -Infinity 
   
-//Input: -10      => Output: -10° Celsius In Fahrenheit:14 °F
-//Input: 120      => Outpt: 120° Celsius In Fahrenheit: 248 °F
+//Input: -10      => Output: -10° Celsius In Fahrenheit: 14 °F
+//Input: 120      => Output: 120° Celsius In Fahrenheit: 248 °F
+//Input: NaN or Infinity or -Infinity   => Output: Please enter number other than NaN, +Infinity, -Infinity
 //Input: other than Number   => Output: Expected Number, Got something else
   
