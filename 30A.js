0107b9f2ae615f48f6740eab58c4a04288a574ca
Akhil <uday.sivapuramvenkata@mobigesture.com>
2019-02-28 10:49:18 +0530

    Split a given array/object by length/2.

--- 30A.js
+++ 30A.js
@@ -1,11 +1,18 @@
 //split a given array/object by length/2.
-//Expected input type : Array. (Enter the array like [1,2,3,4,5,6,7,8])
+//Expected input type : Array. (Enter the array like [1,2,3,4,5,6,7,8]) and a Object.
 
 var stng = [1,2,3,4,5,6,7,8];
-var len = 4;
-//Tried with different inputs like  "akhil",{"Akhil" : 123},1234567,true...
+var len = (stng.length)/2;
+var obj = {
+  "NAME" : "AKHIL",
+  "AGE" : 22,
+  "HEIGHT" : 6,
+  "RANK" : 1
+}
 
-//This function is to make the split a given array...
+//Tried with different inputs like  {  "NAME" : "AKHIL",  "AGE" : 22,  "HEIGHT" : 6,  "RANK" : 1},"akhil",[1,2,3,4,5,6,7,8],1234567,true... 
+
+//This function is to split the given array...
 const spltArr = (stng, len) => {
     var n = stng.length;
     var tempArr = [];
@@ -17,14 +24,38 @@ const spltArr = (stng, len) => {
     return tempArr;
 }
 
+//This function is to split the  given object...
+const spltObj = (obj) => {
+  var c=0;
+  for(let key in obj)
+    c++;
+  var obj1 = {};
+  var obj2 = {};
+  for(let k in obj){
+    if((Object.keys(obj).indexOf(k) < c/2))
+      obj1[k] = obj[k];
+    else
+      obj2[k] = obj[k]; 
+  }
+  console.log("These are the Splits at (length/2) of given Object ");
+  console.log(obj1);
+  console.log(obj2);
+}
+
 const spltArray = (stng,len) => {
   return ( ( (Array.isArray(stng) && (typeof len == 'number') ) ) ? spltArr(stng, len) : "Failed to Execute because of Wrongly Entered Input"); 
 };
 
+const spltObject = (obj) => {
+  return ( ( ( (typeof obj == 'object') && !(Array.isArray(obj)) ) ) ? spltObj(obj) : console.log("Failed to Execute because of Wrongly Entered Input") ); 
+};
+
 //Calling the function here
 var result = spltArray(stng,len);
+spltObject(obj);
+
 //Printing the results
-console.log("These are the Splits at given length " ); 
+console.log("These are the Splits at (length/2) of given Array " ); 
 if(Array.isArray(result)){
   for(var x in result)
   console.log(result[x]);
@@ -33,14 +64,10 @@ else
   console.log(result);
 
 
-//Expected output when the Given array [1,2,3,4,5,6,7,8] is These are the Splits at given length  (4) [1, 2, 3, 4] (4) [5, 6, 7, 8]
+//Expected output when the Given array [1,2,3,4,5,6,7,8] and a Object is 
+//These are the Splits at (length/2) of given Array  (4) [1, 2, 3, 4] (4) [5, 6, 7, 8]  
+//These are the Splits at (length/2) of given Object {NAME: "AKHIL", AGE: 22} {HEIGHT: 6, RANK: 1}
 
 //Expected output when the Given input is 123 These are the Splits at given length  Failed to Execute because of Wrongly Entered Input
-
-//Expected output when the Given input is {"Akhil" : 123} These are the Splits at given length  Failed to Execute because of Wrongly Entered Input
-
-//Expected output when the Given input is ["akhi",123] These are the Splits at given length  Failed to Execute because of Wrongly Entered Input
-
-//Expected output when the Given input is true These are the Splits at given length  Failed to Execute because of Wrongly Entered Input
 
 //Expected output when the Given input is true These are the Splits at given length  Failed to Execute because of Wrongly Entered Input
