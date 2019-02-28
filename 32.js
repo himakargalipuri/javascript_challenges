67fe24474cab285b11927a55172ef8513030f195
Abhinav <chelluri.abhinav@gmail.com>
2019-02-28 23:41:39 +0530

    Create class, return addition, multiplication, exponential of a given number Var number = new numClass(4) number.add(), number.multi(), number.exp()

--- 32.js
+++ 32.js
@@ -5,8 +5,8 @@
 var n1 = 10, n2 = 2;
 
 const mathOp = (a, b) => {  
-    class numClass{     //crating class 
-      constructor(a,b){     //assigning vales to class variables using constructor
+    class numClass{     //creating class 
+      constructor(a,b){     //assigning values to class variables using constructor
         this.a = a;
         this.b = b;
       }
@@ -21,26 +21,29 @@ const mathOp = (a, b) => {
     "Exponential: " + number.exp());
 }
 
-const  init = () => {
-    (n1 == "" || n2 == "" || !(typeof n1 == 'number') || !(typeof n2 == 'number')) ?
-     console.log("Expected Numbers as input, Got something else") : mathOp(n1, n2);
-    //checking the input value
+
+const check = (n1, n2) => {    //checking various inputs types
+  ((isNaN(n1)) || (n1 == Infinity) || (n1 == -Infinity) || (isNaN(n2)) || (n2 == Infinity) || (n2 == -Infinity)) ? 
+    console.log("Please enter numbers other than NaN, +Infinity, -Infinity") : 
+    mathOp(n1, n2);
 }
 
+//checking various inputs
+const  init = () => {
+  !(typeof n1 == 'number') || !(typeof n2 == 'number') ? console.log("Expected Number as input, Got something else") : check(n1, n2);
+}
 init();
-
      
 //Conclusion: 
 // Checked with n1 = 10 n2 = 2, n1 = "10" n2 = 2,  n1 = "10" n2 = "2",
-//              n1 = true n2= false,  {"n1": "5", "n2": "6"} 
+//              n1 = true n2= false,  {"n1": "5", "n2": "6"}, null, undefined, "", NaN, Infinity, -Infinity  
   
 // Input: n1 = 10, n2 = 2       
 // => Output: Addition: 12
 //            Multiplication: 20
 //            Exponential: 100
-// 
-// Input: other than n1 = Number and n2 = Number  
-// => Output: Expected Numbers as input, Got something else
+//Input: n1 or n2 = NaN or Infinity or -Infinity   => Output: Please enter numbers other than NaN, +Infinity, -Infinity
+// Input: other than n1 = Number and n2 = Number   => Output: Expected Numbers as input, Got something else
 
 
 
