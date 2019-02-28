298f65aa789edae310e93c968e7d88036d12f3bf
Abhinav <chelluri.abhinav@gmail.com>
2019-02-28 23:33:54 +0530

    Validate if given input is array

--- 17.js
+++ 17.js
@@ -1,12 +1,13 @@
 //17. Validate if given input is array
 var arr = [1,2,3] 
-console.log(((x) => {
-    return (x instanceof Array) ? "It is Array" : "Not an array"
-})(arr));
+const isItArray = (x) => {
+    return (x instanceof Array) ? "It is an Array" : "Not an array"
+}
+console.log(isItArray(arr));
 
 //conclusion: 
-  // Checked with [1,2,3], "1,2,3", true/false, {1: "a", 2: "3", 4: "r"} 
+  // Checked with arr = [1,2,3], "1,2,3", true/false, {1: "a", 2: "3", 4: "r"}, null, undefined, "", NaN, Infinity, -Infinity 
   
-  //Input: [1,2,3]            => Output: It is Array
+  //Input: [1,2,3]            => Output: It is an Array
   //Input: other than array   => Output: Not an array
   
