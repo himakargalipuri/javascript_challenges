0ba35215905ece6373b3b684fd23137ca7b6b761
Abhinav <chelluri.abhinav@gmail.com>
2019-02-28 10:45:23 +0530

    Capitalise every alternative letter from a given string, ignore spaces

---
+++ 21.js
@@ -0,0 +1,33 @@
+// 21. Capitalise every alternative letter from a given string, ignore spaces 
+
+var str = "Hello world!"
+
+const toAltUpper = (str) => {
+  var arr = str.split(""), c=0
+  for(let i in arr){
+    if(arr[i] == " "){continue}   //skipping the loop if space occured
+    else{
+      c++ 
+      if(c==2)    //Tracking the 2nd character by ignoring space
+      {
+        arr[i] = arr[i].toUpperCase();   //Replacing the 2nd character with Uppercase
+        c=0;
+      }
+    }
+  }
+  console.log(arr.join().replace(/,/g,""))
+}
+
+const  init = () => {
+  ((str == "") || !(typeof str == 'string')) ? console.log("Expected string, Got something else") : toAltUpper(str);
+  //checking the input value
+}
+init();
+
+        
+  //conclusion: 
+  // Checked with ""Hello world!", ["welcome", "abort"], true, 1234  {"ahello: 1, "world": "asdf", "c": true, "ak": undefined} 
+  
+  //Input: "Hello world!"       => Output: WElCoMe To JaVaScRiPt!
+  //Input: other than string    => Outpt: Expected string, Got something else
+  
