61de1324ef5d74f4b1cb7718d2001f2a75a36638
Abhinav <chelluri.abhinav@gmail.com>
2019-02-28 23:39:49 +0530

    Return the index of all capital letters

--- 29.js
+++ 29.js
@@ -1,26 +1,27 @@
 //29. Return the index of all capital letters 
 
 var str = "hElo WorLD!";
-const  init = () => {
-    ((str == "") || !(typeof str == 'string')) ? console.log("Expected String, Got something else") : console.log(capitalIndex(str));
-}
-//driver code
+
 const capitalIndex = (st) => {
   var res = "";
     for (let i in st) {
-      if (st[i].match(/[A-Z]/)) {    //matching every character with A-Z
+      if (st[i].match(/[A-Z]/g)) {    //matching every character with A-Z
         res += st.indexOf(st.charAt(i)) + " ";
         // storing indexes in res
       }
     }
-  return res;
+  return "Index of Capital Letters: " + res;
 }
+const  init = () => {
+  !(typeof str == 'string') ? console.log("Expected String, Got something else") : console.log(capitalIndex(str));
+}
+
 init();
 
      
 //Conclusion: 
-// Checked with "hElo WorLD!", 1234,  [1234], true/false,  {"a": "5", "b": "6", "c": "8"} 
+// Checked with "hElo WorLD!", 1234,  [1234], true/false,  {"a": "5", "b": "6", "c": "8"}, null, undefined, "", NaN, Infinity, -Infinity 
   
-//Input: "hElo WorLD!"       => Output: 1 5 8 9
+//Input: "hElo WorLD!"       => Output: Index of Capital Letters: 1 5 8 9
 //Input: other than String   => Output: Expected String, Got something else
   
