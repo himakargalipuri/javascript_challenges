6107a6142bc625cc1bf3f266cba134cd88b187f2
Abhinav <chelluri.abhinav@gmail.com>
2019-02-28 23:38:52 +0530

    Reverse a string

--- 27.js
+++ 27.js
@@ -1,14 +1,15 @@
 //27. Reverse a string
+
 var str = "ak121786";
 
 const myReverse = () => {
-(str == "" || !(typeof str == 'string')) ? console.log("Expected String, Got something else.") : console.log(str.split("").reverse().join().replace(/,/g, ""));
-//changing the String to array, then reverse then back to String and replacing , with ""
+!(typeof str == 'string') ? console.log("Expected String, Got something else.") : console.log(str.split("").reverse().join().replace(/,/g, ""));
+//changing the String to array, then reverse then back to String and replacing "," with ""
 }
 
 myReverse()
 //conclusion: 
-// Checked with "ak121786", [1234,"ak"], true/false,  1234, {"a": 123, "b": "abcd", "c": "128"} 
+// Checked with "ak121786", [1234,"ak"], true/false,  1234, {"a": 123, "b": "abcd", "c": "128"}, null, undefined, "", NaN, Infinity, -Infinity 
 //Input: "ak121786"          => Output: 687121ka
 //Input: other than String   => Output: Expected String, Got something else.
   
