ade1b6fb175d5e72d755adcd5e4896d5ea6f2923
Abhinav <chelluri.abhinav@gmail.com>
2019-02-28 18:29:33 +0530

    Given an array of mixed value types. Return an array of their types [1,’adsf’, [a,b], {}, undefined, true] = [number, string, array, object, undefined, boolean]

--- 13.js
+++ 13.js
@@ -20,7 +20,7 @@ init();
   //When returning the typeof [a,b] it give output as 'object' because Arrays in JavaScript are special case of objects (numbered index objects)
   
   // Checked with [1, "adsf", {}, undefined, ["a", "b"], true]], 1, "adsf", {}, undefined, ["a", "b"], true",  
-  //      {"a": 1, "b": "asdf" "c": {}, "b": true, "a": undefined}, null, undefined, "", NaN 
+  //      {"a": 1, "b": "asdf" "c": {}, "b": true, "a": undefined}, null, undefined, "", NaN, Infinity, -Infinity
   //Input: [1, "adsf", {}, undefined, ["a", "b"], true]]     => Output: [ 'number', 'string', 'object', 'undefined', 'array', 'boolean' ]
   //Input: other than array        => Outpt: Expected array, Got something else
   
