af31cb3d0a2422cc5ffb52f41415fa6bb1fe9d45
Abhinav <chelluri.abhinav@gmail.com>
2019-02-28 23:34:14 +0530

    Find sum of numbers between 1 to n. Dont use loop.

--- 18.js
+++ 18.js
@@ -1,17 +1,25 @@
 //18. Find sum of numbers between 1 to n. Dont use loop.
 
-var n = 3;
+var num = 5;
 
 //checking valid input
-if(!(typeof n == 'number') || n==""){  //checking if number is valid or not
-  console.log("Expected Number, Got something else");
-} else if(n<0){ console.log("Please input a positive number"); }
-else{ console.log("Sum = " + (n * (n+1)/2)) }     //Basic Mathematical Formula
+const sumOfNumbers = (n) => {
+  return !(n<0) ? `Sum = ${n * (n+1)/2}` : `Sum = -${(-n * (-n+1)/2) - 1}`
+}
 
+const check = (num) => {  //checking various number types
+  (isNaN(num) || (num == Infinity) || (num == -Infinity)) ? console.log("Please enter number other than NaN, +Infinity, -Infinity") : console.log(sumOfNumbers(num));
+}
+//checking various inputs
+const  init = () => {
+  !(typeof num == 'number') ? console.log("Expected Number as input, Got something else") : check(num);
+}
+init();
   //conclusion: 
-  // Checked with 5, -5, "5", [5],  , true/false,  {5: 5} 
+  // Checked with 5, -5, "5", [5], true/false,  {5: 5}, null, undefined, "", NaN, Infinity, -Infinity  
   
   //Input: 5     => Output: Sum = 15
-  //Input: -5    => Output: Please input a positive number
-  //Input: other than number   => Outupt: Expected Number, Got something else
+  //Input: -5    => Output: Sum = -14
+  //Input: NaN or Infinity or -Infinity       => Output: Please enter number other than NaN, Infinity, -Infinity
+  //Input: other than number   => Outupt: Expected Number as input, Got something else
   
