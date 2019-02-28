5c0afc9163903732977d0cd0f4f2d87592af3141
Abhinav <chelluri.abhinav@gmail.com>
2019-02-28 23:38:28 +0530

    Return 7 days from now date as well as 2 days ago date from now

--- 26.js
+++ 26.js
@@ -1,8 +1,8 @@
 //26. Return 7 days from now date as well as 2 days ago date from now 
 
-const getFuturePast = () =>{
+const getFuturePast = () => {
   console.log("7 days from now date: " + new Date(new Date().setDate(new Date().getDate() + 7)) + "\n" + "\
-  2 days ago date from now: "+ new Date(new Date().setDate(new Date().getDate() - 2)))
+2 days ago date from now: "+ new Date(new Date().setDate(new Date().getDate() - 2)))
   //using the inbuilt date set and get methods to obtain the rquired dates.
 }
 
