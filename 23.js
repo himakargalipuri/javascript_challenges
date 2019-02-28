824a23518a6dea9cfb6be78b321bf11ed2ced761
Abhinav <chelluri.abhinav@gmail.com>
2019-02-28 23:36:58 +0530

    Given annual salary numbers, calculate income tax rate and income tax amount for Individuals below 60 years aSal = Annual Salary, tr = Tax Rate, ita = Income Tax Amount in INR, itr = Income Tax Rate

--- 23.js
+++ 23.js
@@ -1,42 +1,40 @@
-// 23. Given annual salary numbers, calculate income tax rate and income tax 
-// amount for Individuals below 60 years
-
+// 23. Given annual salary numbers, calculate income tax rate and income tax amount for Individuals below 60 years
 //aSal = Annual Salary, tr = Tax Rate, ita = Income Tax Amount in INR, itr = Income Tax Rate
+
 var aSal = 1200000, ita=0, itr=0;
-const myITSlabs = () => {
-    if(aSal <= 250000){
-        ita = 0 * aSal; 
-        itr = (ita * 100)/aSal
+const myITSlabs = (aSal) => {
+    var r = 0;
+    if(aSal<0) {
+        return "Please enter positive Annual Salary"
     }
-    else if(aSal >= 250000 && aSal <= 500000){
-        ita = (5/100) * aSal; 
-        itr = (ita * 100)/aSal
-    }
-    else if(aSal >= 500000 && aSal <= 1000000){
-        ita = (20/100) * aSal; 
-        itr = (ita * 100)/aSal
-    }
-    else{
-        ita = (30/100) * aSal; 
-        itr = (ita * 100)/aSal
-    }
-    console.log("Annual Salary: ", aSal +" INR");
-    console.log("Income Tax Amount: ", ita +" INR");
-    console.log("Income Tax Rate:   " + itr + "%");
+    //Assigning Rate of Interest based on Annual Salary
+    if(aSal <= 250000){ r = 0; }
+    else if(aSal >= 250000 && aSal <= 500000){ r = 5/100 }
+    else if(aSal >= 500000 && aSal <= 1000000){ r = 20/100 }
+    else{ r = (30/100) }
+
+    ita = r * aSal; 
+    itr = (ita * 100)/aSal
+    return "Annual Salary: "+ aSal +" INR" + "\nIncome Tax Amount: " + ita +" INR\nIncome Tax Rate:   " + itr + "%"
 }
 
-//checking various inputs
-const init = () =>{
-    ((aSal == "") || !(typeof aSal == 'number')) ? console.log("Expected Number, Got something else") : myITSlabs();
-}
 
-init()
+const check = (aSal) => {
+    (isNaN(aSal) || (aSal == Infinity) || (aSal == -Infinity)) ? console.log("Please enter number other than NaN, +Infinity, -Infinity") : console.log(myITSlabs(aSal));
+  }
+  
+  //checking various inputs
+  const  init = () => {
+    !(typeof aSal == 'number') ? console.log("Expected Number as input, Got something else") : check(aSal);
+  }
+  init();
+
 
   //conclusion: 
-  // Checked with -1200000, 150000, 300000, 700000, 1200000, "150000", [120000] true/false,  {"a": 5000, "b": 1200000, "c": 700000} 
+  // Checked with -1200000, 150000, 300000, 700000, 1200000, 3000000 "150000", [120000] true/false,  
+  //                 {"a": 5000, "b": 1200000, "c": 700000}, null, undefined, "", NaN, Infinity, -Infinity 
   
-  //Input: -1200000    => Output: Income Tax Amount: 0 INR
-  //                              Income Tax Rate: 0%
+  //Input: -1200000    => Output: Please enter positive Annual Salary
 
   //Input: 150000      => Output: Income Tax Amount: 0 INR
   //                              Income Tax Rate: 0%
@@ -47,8 +45,9 @@ init()
   //Input: 700000      => Output: Income Tax Amount: 140000 INR
   //                              Income Tax Rate: 20%
 
-  //Input: 300000      => Output: Income Tax Amount: 360000 INR
+  //Input: 1200000     => Output: Income Tax Amount: 360000 INR
   //                              Income Tax Rate: 30%
 
-  //Input: other than Number      => Output: Expected Number, Got something else
+  //Input: NaN or Infinity or -Infinity   => Output: Please enter number other than NaN, +Infinity, -Infinity
+  //Input: other than Number              => Output: Expected Number as input, Got something else
   
